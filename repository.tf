303fa7ef9a8e2755ac7045d6a6111f1680fafa20
Romain Cambonie <romain.cambonie@gmail.com>
2023-06-04 09:59:32 +0200

    fix: hardcoded organization

--- repository.tf
+++ repository.tf
@@ -1,7 +1,7 @@
 module "repository" {
   source = "github.com/codingones-terraform-modules/github-repository"
 
-  github_organization                  = "codingones-github-templates"
+  github_organization                  = var.github_organization
   github_repository                    = var.github_repository
   github_repository_topics             = var.github_repository_topics
   allow_force_pushes_to_default_branch = var.allow_force_pushes_to_default_branch
